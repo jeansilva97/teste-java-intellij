71befde3de8fae336017779320caf6e21abd7ab6
Jean José <jean_profile@hotmail.com>
2021-07-29 14:24:32 -0300

    first commit

---
+++ src/com/br/dio/primeiroprograma.java
@@ -0,0 +1,14 @@
+package com.br.dio;
+
+
+import com.br.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        gato Gato = new gato();
+        System.out.println(Gato);
+       /* int a=5;
+        int b=6;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+}
